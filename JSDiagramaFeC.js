3eab8d904d31dfbd2c3cda18d7fd5387d258059b
Amanda <asanjuan1806@gmail.com>
2021-09-03 13:29:34 +0200

    diagrama del FeC v4

--- JSDiagramaFeC.js
+++ JSDiagramaFeC.js
@@ -1,6 +1,59 @@
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if( ev.target.tagName === 'LI') {
-     ev.target.classList.toggle('done');
+var list1 = document.querySelector('.u1');
+list1.addEventListener('click', function(ev) {
+  if( ev.target.className === 'li1') {
+     ev.target.classList.toggle('right');
+  }
+  if( ev.target.className === 'li2') {
+     ev.target.classList.toggle('wrong');
+  }
+}, false);
+
+var list2 = document.querySelector('.u2');
+list2.addEventListener('click', function(ev) {
+  if( ev.target.className === 'li1') {
+     ev.target.classList.toggle('right');
+  }
+  if( ev.target.className === 'li2') {
+     ev.target.classList.toggle('wrong');
+  }
+}, false);
+
+var list3 = document.querySelector('.u3');
+list3.addEventListener('click', function(ev) {
+  if( ev.target.className === 'li1') {
+     ev.target.classList.toggle('right');
+  }
+  if( ev.target.className === 'li2') {
+     ev.target.classList.toggle('wrong');
+  }
+}, false);
+
+var list4 = document.querySelector('.u4');
+list4.addEventListener('click', function(ev) {
+  if( ev.target.className === 'li1') {
+     ev.target.classList.toggle('right');
+  }
+  if( ev.target.className === 'li2') {
+     ev.target.classList.toggle('wrong');
+  }
+}, false);
+
+var list5 = document.querySelector('.u5');
+list5.addEventListener('click', function(ev) {
+  if( ev.target.className === 'li1') {
+     ev.target.classList.toggle('right');
+  }
+  if( ev.target.className === 'li2') {
+     ev.target.classList.toggle('wrong');
+  }
+}, false);
+
+var list6 = document.querySelector('.u6');
+list6.addEventListener('click', function(ev) {
+  if( ev.target.className === 'li1') {
+     ev.target.classList.toggle('right');
+  }
+  if( ev.target.className === 'li2') {
+     ev.target.classList.toggle('wrong');
   }
 }, false);
